1fff38afc9ff553d364f77f4456123d244fe243f
Conor Driscoll <conor@driscoll66.com>
2018-07-13 13:11:08 -0400

    one more
    First page draft done. start safety page.

--- Olivia-GM/src/assets/js/app.js
+++ Olivia-GM/src/assets/js/app.js
@@ -36,6 +36,56 @@ $(function() {
 var waypoint = new Waypoint({
   element: document.getElementById('waypoint'),
   handler: function(direction) {
-    console.log('Scrolled to waypoint!')
   }
 })
+
+
+$(document).ready( function() {
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  var vpHeight = ($(".upperquad").height());
+  var presDis = $('.sticky-container').css("display");
+
+  if (scroll >= vpHeight) {
+    if (presDis == "none" ) {
+  $('.sticky-container').css("display", "block");
+}
+  }
+  else{
+    if (presDis == "block" ) {
+  $('.sticky-container').css("display", "none");
+}
+  }
+
+
+});
+});
+
+//play audio
+
+var continuousElements = document.getElementsByClassName('slide')
+for (var i = 0; i < continuousElements.length; i++) {
+  new Waypoint({
+    element: continuousElements[i],
+    handler: function(direction) {
+      if(direction == "down")
+      var snd = ('#' + this.element.id + "-sound");
+      $(snd).trigger('play');
+    }
+  })
+}
+
+//Audio mute
+
+
+$("video").prop('muted', true);
+
+  $("#mute-audio").click( function (){
+    if( $("audio").prop('muted') ) {
+          $("audio").prop('muted', false);
+          $(this).removeClass('fi-volume-strike').addClass('fi-volume');
+    } else {
+      $("audio").prop('muted', true);
+      $(this).removeClass('fi-volume').addClass('fi-volume-strike');
+    }
+  });
